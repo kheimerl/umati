85636162b0752c4cf0fc4135d15edc25a97ad8c3
kheimerl <kheimerl@ef6d2edd-3247-4f8e-8874-c4ba0b0cbb49>
2010-11-14 02:31:37 +0000

    logic starting
    git-svn-id: svn+ssh://vmphone2.cs.berkeley.edu/home/kheimerl/REPOS/SVN/umati@12 ef6d2edd-3247-4f8e-8874-c4ba0b0cbb49

--- umati/UmatiMainWindow.py
+++ umati/UmatiMainWindow.py
@@ -1,6 +1,9 @@
 from PyQt4 import QtGui
 import UmatiMainView
 import UmatiChooserWidget
+import UmatiVendWidget
+import UmatiMathTaskWidget
+import UmatiSurveyTaskWidget
 
 class MainWindow(QtGui.QMainWindow):
     
@@ -11,3 +14,28 @@ class MainWindow(QtGui.QMainWindow):
         
         #setup other parts
         self.chooser = UmatiChooserWidget.ChooserGui(self.ui.BodyFrame)
+        self.vend = UmatiVendWidget.VendGui(self.ui.BodyFrame)
+        self.math_task = UmatiMathTaskWidget.MathTaskGui(self.ui.BodyFrame)
+        self.survey_task = UmatiSurveyTaskWidget.SurveyTaskGui(self.ui.BodyFrame)
+        self.setChooserVisible()
+
+    def __setItemVisible(self, item, others):
+        item.show()
+        for other in others:
+            other.hide()
+
+    def setChooserVisible(self):
+        self.__setItemVisible(self.chooser, [self.vend, self.math_task,
+                                             self.survey_task])
+
+    def setVendVisible(self):
+        self.__setItemVisible(self.vend, [self.chooser, self.math_task,
+                                          self.survey_task])
+
+    def setMathTaskVisible(self):
+        self.__setItemVisible(self.math_task, [self.chooser, self.vend,
+                                               self.survey_task])
+
+    def setSurveyTaskVisible(self):
+        self.__setItemVisible(self.survey_task, [self.chooser, self.vend,
+                                                 self.math_task])
--- umati/UmatiSurveyWidget.py
+++
@@ -1,9 +0,0 @@
-from PyQt4 import QtGui
-import UmatiSurveyView
-
-class SurveyGui(QtGui.QWidget):
-
-    def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self, parent)
-        self.ui = UmatisurveyView.Ui_Form()
-        self.ui.setupUi(self)
