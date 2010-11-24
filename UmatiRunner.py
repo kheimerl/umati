2de2232470a919d05607c8437e469b18b6aa4088
kheimerl <kheimerl@ef6d2edd-3247-4f8e-8874-c4ba0b0cbb49>
2010-11-24 02:59:22 +0000

    basic logging added
    git-svn-id: svn+ssh://vmphone2.cs.berkeley.edu/home/kheimerl/REPOS/SVN/umati@31 ef6d2edd-3247-4f8e-8874-c4ba0b0cbb49

--- UmatiRunner.py
+++ UmatiRunner.py
@@ -1,8 +1,36 @@
 #!/usr/bin/python
 
 import sys
+import logging
+import getopt
 from PyQt4 import QtGui
-from umati import UmatiMainWindow
+from umati import UmatiMainWindow, Util
+
+opts, args = getopt.getopt(sys.argv[1:], 
+                           "l:h", ["help", "logLevel="])
+
+def usage():
+    print ("The Umati Vending Machine User Interface")
+    print ("-h | --help Show this message")
+    print ("-l | --logLevel=[DEBUG|INFO|WARNING|ERROR|CRITICAL] Default is INFO")
+    exit(2)
+
+log_level = "INFO"
+
+for o,a in opts:
+    if (o == "-h" or o == "--help"):
+        usage()
+    elif o in ("-l", "--logLevel"):
+        log_level = a
+
+log_level = Util.getLogLevel(log_level)
+if not (log_level):
+    print ("Invalid Log Level")
+    usage()
+
+logging.basicConfig(filename=Util.LOG_LOC, 
+                    level=log_level, 
+                    format=Util.LOG_FORMAT)
 
 app = QtGui.QApplication(sys.argv)
 mw = UmatiMainWindow.MainWindow()
---
+++ umati/Util.py
@@ -0,0 +1,18 @@
+import logging
+
+LOG_LOC = "umati.log"
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+def getLogLevel(log_level):
+    if (log_level == "DEBUG"):
+        return logging.DEBUG
+    elif(log_level == "INFO"):
+	return logging.INFO
+    elif(log_level == "WARNING"):
+        return logging.WARNING
+    elif(log_level == "ERROR"):
+        return logging.ERROR
+    elif(log_level == "CRITICAL"):
+        return logging.CRITICAL
+    else:
+        return None
