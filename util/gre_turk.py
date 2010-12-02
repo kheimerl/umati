06ed8624ea388dafadbffe5101c28cd051b95408
kuangc <kuangc@ef6d2edd-3247-4f8e-8874-c4ba0b0cbb49>
2010-12-02 08:52:50 +0000

    split into 5 files
    git-svn-id: svn+ssh://vmphone2.cs.berkeley.edu/home/kheimerl/REPOS/SVN/umati@66 ef6d2edd-3247-4f8e-8874-c4ba0b0cbb49

--- util/gre_turk.py
+++ util/gre_turk.py
@@ -11,22 +11,31 @@ cur = ''
 print('<h3>Answer these questions</h3>')
 
 count = 0
+outfile = open('gre0.html', 'w')
 for line in open(sys.argv[1]):
+    if count == 4:
+        pass
     y = ANS_PARSE.match(line)
     if (DELIM_RE.search(line)):
-        print("<br>\n")
+        if count % 5 == 0:
+            outfile.close()
+            s = str(count/5)
+            outfile = open('gre'+s+'.html', 'w')
     elif (y):
         if (cur != ''):
             
-            z = HEADER.match(cur)
+            # z = HEADER.match(cur)
             
-            # print('<p>%s</p>' % cur.strip())
-            print('<p>%s</p>' % z.group(2))
+            outstr = '<p>%s</p>' % cur.strip()
+            print(outstr)
+            outfile.write(outstr+'\n')
+            # print('<p>%s</p>' % z.group(2))
             cur = ''
             count += 1
         val = y.group(2)
-        print('<input type="checkbox" value="%s" name="%d"/>%s<br>' % (val, count, val))
+        outstr = '<input type="checkbox" value="%s" name="%d"/>%s<br>' % (val, count, val)
+        print(outstr)
+        outfile.write(outstr+'\n')
     else:
         cur += line
-
-print('<br>')
+outfile.close()
