e7f750cf37fdc46e7d0fcd9205a16ee736b3dc80
kheimerl <kheimerl@ef6d2edd-3247-4f8e-8874-c4ba0b0cbb49>
2011-03-28 01:42:04 +0000

    new zoomable stuff
    git-svn-id: svn+ssh://vmphone2.cs.berkeley.edu/home/kheimerl/REPOS/SVN/umati@171 ef6d2edd-3247-4f8e-8874-c4ba0b0cbb49

--- umati/UmatiWidget.py
+++ umati/UmatiWidget.py
@@ -19,27 +19,81 @@ class Widget(QtGui.QWidget):
         
 #utility classes, extending QtStuff
 
-class PanningTextBrowser(QtGui.QTextBrowser):
-    
-    def __init__(self, parent=None):
-        QtGui.QTextBrowser.__init__(self, parent)
+class ZoomAndPannable():
+
+    DOUBLE_CLICK_TIME = 0.25
+    RESET_TIME = 0.5
+
+    def __init__(self, start):
+        self.press_count = 0 #count number of presses
+        self.last_press = 0 #last time we completed a press event
+        self.press_time = 0 #last time we pressed down
+        self.zoomed = start
         self.pressed = None
 
     def mousePressEvent(self, event):
         self.pressed = (event.x(), event.y())
- 
+        self.press_time = time.time() 
+        if (self.press_time - self.last_press > ZoomAndPannable.RESET_TIME):
+            self.press_count = 0
+
     def mouseMoveEvent(self, event):
         if (self.pressed):
+            self.pressed = (event.x(), event.y())
+
+    def mouseReleaseEvent(self, event):
+        self.pressed = None
+        if (time.time() - self.press_time < 
+            PanningWebBrowser.DOUBLE_CLICK_TIME):
+            self.press_count += 1
+            self.last_press = time.time()
+        if (self.press_count == 2):
+            self.press_count = 0
+            if (self.zoomed):
+                self.zoom(False)
+            else:
+                self.zoom(True)
+
+    def zoom(self, zoom):
+        self.zoomed = zoom
+
+    def pressed(self):
+        return self.pressed
+
+class PanningTextBrowser(QtGui.QTextBrowser, ZoomAndPannable):
+
+    def __init__(self, parent=None):
+        QtGui.QTextBrowser.__init__(self, parent)
+        ZoomAndPannable.__init__(self, False)
+
+    def mousePressEvent(self, event):
+        ZoomAndPannable.mousePressEvent(self, event)
+
+    def mouseMoveEvent(self, event):
+        if (ZoomAndPannable.pressed(self)):
             hbar = self.horizontalScrollBar()
             vbar = self.verticalScrollBar()
             if (hbar):
                 hbar.setValue(hbar.value() + (self.pressed[0] - event.x()))
             if (vbar):
                 vbar.setValue(vbar.value() + (self.pressed[1] - event.y()))
-            self.pressed = (event.x(), event.y())
+        ZoomAndPannable.mouseMoveEvent(self, event)
+
+    def zoom(self, zoom):
+        ZoomAndPannable.zoom(self, zoom)
+        if (zoom):
+            self.zoomIn(2)
+        else:
+            self.zoomOut(2)
  
+    def __changeFontSize(self, size):
+        self.selectAll()
+        font = self.currentFont()
+        font.setPointSize(size)
+        self.setCurrentFont(font)
+
     def mouseReleaseEvent(self, event):
-        self.pressed = None
+        ZoomAndPannable.mouseReleaseEvent(self, event)
 
     def show(self):
         QtGui.QTextBrowser.show(self)
@@ -48,57 +102,36 @@ class PanningTextBrowser(QtGui.QTextBrowser):
             if (bar):
                 bar.hide()
 
-class PanningWebBrowser(QtWebKit.QWebView):
-
-    DOUBLE_CLICK_TIME = 0.25
-    RESET_TIME = 0.5
+class PanningWebBrowser(QtWebKit.QWebView, ZoomAndPannable):
 
     def __init__(self, parent=None):
         QtWebKit.QWebView.__init__(self, parent)
-        self.pressed = None
-        self.press_count = 0 #count number of presses
-        self.last_press = 0 #last time we completed a press event
-        self.press_time = 0 #last time we pressed down
+        ZoomAndPannable.__init__(self, False)
         self.page().mainFrame().setZoomFactor(1.0)
         self.page().mainFrame().setScrollBarPolicy(QtCore.Qt.Horizontal, 
                                                    QtCore.Qt.ScrollBarAlwaysOff)
         self.page().mainFrame().setScrollBarPolicy(QtCore.Qt.Vertical, 
                                                    QtCore.Qt.ScrollBarAlwaysOff)
-        self.zoomed = False
 
     def mousePressEvent(self, event):
-        self.pressed = (event.x(), event.y())
-        self.press_time = time.time()
-        if (self.press_time - self.last_press > PanningWebBrowser.RESET_TIME):
-            self.press_count = 0
+        ZoomAndPannable.mousePressEvent(self, event)
 
     def mouseMoveEvent(self, event):
-        if (self.pressed):
+        if (ZoomAndPannable.pressed(self)):
             frame = self.page().mainFrame()
             if (frame):
                 cur_pos = frame.scrollPosition()
                 frame.setScrollPosition(QtCore.QPoint(
                         cur_pos.x() + self.pressed[0] - event.x(),
                         cur_pos.y() + self.pressed[1] - event.y()))
-            self.pressed = (event.x(), event.y())
+        ZoomAndPannable.mouseMoveEvent(self, event)
 
     def mouseReleaseEvent(self, event):
-        self.pressed = None
-        if (time.time() - self.press_time < PanningWebBrowser.DOUBLE_CLICK_TIME):
-            self.press_count += 1
-            self.last_press = time.time()
-        if (self.press_count == 2):
-            self.press_count = 0
-            if (self.zoomed):
-                self.page().mainFrame().setZoomFactor(1.0)
-                self.zoomed = False
-            else:
-                frame = self.page().mainFrame()
-                frame.setZoomFactor(2.0)
-                frame_size = frame.geometry()
-                frame.setScrollPosition(QtCore.QPoint(
-                        int((float(event.x())/frame_size.width()) * 
-                            frame_size.width()),
-                        int((float(event.y())/frame_size.height()) * 
-                            frame_size.height())))
-                self.zoomed = True
+        ZoomAndPannable.mouseReleaseEvent(self, event)
+
+    def zoom(self, zoom):
+        ZoomAndPannable.zoom(self, zoom)
+        if (zoom):
+            self.page().mainFrame().setZoomFactor(1.0)
+        else:
+            self.page().mainFrame().setZoomFactor(2.0)
