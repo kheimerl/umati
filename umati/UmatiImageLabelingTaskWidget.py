14911dc9df55af8d9db3b99141ee58a67700963f
kheimerl <kheimerl@ef6d2edd-3247-4f8e-8874-c4ba0b0cbb49>
2012-03-21 07:12:51 +0000

    image labeler working
    git-svn-id: svn+ssh://vmphone2.cs.berkeley.edu/home/kheimerl/REPOS/SVN/umati@258 ef6d2edd-3247-4f8e-8874-c4ba0b0cbb49

--- umati/UmatiImageLabelingTaskWidget.py
+++ umati/UmatiImageLabelingTaskWidget.py
@@ -1,55 +1,53 @@
 import sys
 import pickle
-import os.path
+import os
+import functools
 
 from PyQt4 import QtGui, uic, QtCore
 from PyQt4.QtGui import *
 import logging, xml.dom.minidom, random, re
 from . import UmatiMessageDialog, UmatiWidget, UmatiTask
 
-
-
 class ImageLabelingTask(UmatiTask.Task):
 
-    def __init__(self, head, value):
+    def __init__(self, head, location):
         UmatiTask.Task.__init__(self, head)
         self.log = logging.getLogger("umati.UmatiImageLabelingTaskWidget.ImageLabelingTask")
-        self.value = value
-        tagFile=[]
-        images = []
-        self.image = None
-		
-			
-        for image in head.getElementsByTagName("label"):
-            images.append((image.getAttribute("url"), image.getAttribute("tag")))
-        #completed = completedTask
-        random.shuffle(images)
-        
-		#shortcut for everythingabove for testing:
-        userCompleted = self.controller.get_completed_tasks(self.getType())
-        
-
-        temp_set = set()
-        for i in userCompleted:
-            temp_set.add(i[0])
-        for i in images:
-            if (i[0] not in temp_set):
-                 (self.image, self.tagFile) = i
-                 break
+        self.images = []
+        self.loc = location
+        self.value = int(head.getAttribute("value"))
+        for f in os.listdir(self.loc):
+            #all non txt (tag) files
+            if (f[-4:] != ".txt" and f[0] != "."):
+                self.images.append(f)
+        self.randomize()
 	      
     def addTag(self, tag):
-        #to do: write tags back to file.
-        f=open(self.tagFile,"a")
-        f.write(tag + "\n")
+        f = open(self.get_tag_file(), 'a')
+        f.write (" " + tag)
         f.close()
         
     def getTags(self):
-        # read the file, return the contents
-        f = open(self.tagFile,'r')
-        tagString = f.read()
-        f.close()
-        return tagString
+        #if it doesn't exist, we have to create it
+        tagFile = self.get_tag_file()
+        if (not os.path.exists(tagFile)):
+            open(tagFile, 'w').close()
+        f = open(tagFile, 'r')
+        return f.read()
+    
+    def randomize(self):
+        self.index = random.randint(0, len(self.images)-1)
  
+    def next(self):
+        self.index += 1
+        if (self.index >= len(self.images)):
+            self.index = 0
+
+    def prev(self):
+        self.index -= 1
+        if (self.index < 0):
+            self.index = len(self.images) - 1
+
     def submit(self):
         return True
     
@@ -60,7 +58,7 @@ class ImageLabelingTask(UmatiTask.Task):
         return self.value
 
     def getName(self):
-        return self.image
+        return None
 
     def getType(self):
         return "Image_Labeling"
@@ -69,9 +67,11 @@ class ImageLabelingTask(UmatiTask.Task):
         return "NONE"
 
     def get_image(self):
-        return self.image
+        return self.loc + self.images[self.index]
 
-   
+    def get_tag_file(self):
+        return self.get_image() + ".txt"
+
     
 UI_FILE = 'umati/UmatiImageLabelingTaskView.ui'
 
@@ -83,38 +83,68 @@ class TaskGui(UmatiWidget.Widget):
         self.conf = conf
         self.ui = uic.loadUiType(UI_FILE)[0]()
         self.ui.setupUi(self)
-        self.value = int(conf.getAttribute("value"))
         self.name = conf.getAttribute("name")
+        self.loc = self.conf.getAttribute("location")
+        self.cur_task = ImageLabelingTask(self.conf, self.loc)
         self.image_browser = UmatiWidget.PanningWebBrowser(self)
+        self.image_browser.setMinimumHeight(250)
         self.image_browser.setMaximumHeight(250)
-        self.ui.mainLayout.insertWidget(0,self.image_browser)
-        self.ui.submitButton.clicked.connect(self.addText)
-        #self.ui.submitButton.clicked.connect(self.addTag)
-
-    #def addTag(self, tag):
-    #    self.tags += tag
+        self.ui.browser_layout.insertWidget(1,self.image_browser)
+        self.ui.previous.clicked.connect(self.prev)
+        self.ui.next.clicked.connect(self.next)
+        self.setupKeyboard()
 
     def addText(self):
-        text = self.ui.inputEdit.text()
-        self.ui.inputEdit.clear()
-        if (text not in self.ui.listEdit.toPlainText().split("\n")):
+        text = str(self.ui.inputEdit.text()).strip()
+        self.clear()
+        if (text not in self.cur_task.getTags().split(" ")):
             self.cur_task.addTag(text)
             if(self.controller.task_completed(self.cur_task, reset=False)):
-                self.cur_task = ImageLabelingTask(self.conf, self.value)
-                self.show()
+                self.cur_task.next()
+                self.show(False)
         
-    def show(self):
+    def show(self, random=True):
         if (not self.cur_task.get_image()):
-            UmatiMessageDialog.information(self, "You have labeled all the images", title="Notice")
             self.controller.choose_task()
             return
-        self.image_browser.setUrl(QtCore.QUrl(self.cur_task.get_image()))
+        if (random):
+            self.cur_task.randomize()
+        self.image_browser.setUrl(QtCore.QUrl("file://" + self.cur_task.get_image()))
         self.ui.listEdit.setText(self.cur_task.getTags())
         UmatiWidget.Widget.show(self)
 
     def available(self):
-        self.cur_task = ImageLabelingTask(self.conf, self.value)
         return (self.cur_task.get_image() != None)
 
     def getValue(self):
-        return str(self.value)
+        return str(self.cur_task.getValue())
+
+    def prev(self):
+        self.cur_task.prev()
+        self.show(False)
+
+    def next(self):
+        self.cur_task.next()
+        self.show(False)
+
+    def clear(self):
+        self.ui.inputEdit.clear()
+
+    def backsp(self):
+        if (len(self.ui.inputEdit.text()) > 0):
+            self.ui.inputEdit.setText(self.ui.inputEdit.text()[:-1])
+            
+    def buttonPress(self, button):
+        self.ui.inputEdit.setText(self.ui.inputEdit.text() + button)
+            
+    def setupKeyboard(self):
+        #highlevel ones
+        self.ui.submit.clicked.connect(self.addText)
+        self.ui.clear.clicked.connect(self.clear)
+        self.ui.backspace.clicked.connect(self.backsp)
+        #and then the whole keyboard
+        for key in ['q','w','e','r','t','y','u','i','o','p',
+                    'a','s','d','f','g','h','j','k','l','z',
+                    'x','c','v','b','n','m']:
+            but = self.ui.__getattribute__(key)
+            but.clicked.connect(functools.partial(self.buttonPress,key))
