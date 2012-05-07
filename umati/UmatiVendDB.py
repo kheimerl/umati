c262d0ec0fc478762ed934b896284c93c0e49060
kheimerl <kheimerl@ef6d2edd-3247-4f8e-8874-c4ba0b0cbb49>
2012-05-07 15:56:33 +0000

    some debug
    git-svn-id: svn+ssh://vmphone2.cs.berkeley.edu/home/kheimerl/REPOS/SVN/umati@281 ef6d2edd-3247-4f8e-8874-c4ba0b0cbb49

--- umati/UmatiVendDB.py
+++ umati/UmatiVendDB.py
@@ -1,5 +1,6 @@
 import os.path
 import pickle
+import logging
 
 class VendItem:
 
@@ -14,12 +15,15 @@ class VendDB:
 
     def __init__(self, conf):
         path = conf.getAttribute("loc")
+        self.log = logging.getLogger("umati.UmatiVendDB.VendDB")
         if (path == ""):
             path = VendDB.FILE_LOC
         
         if (os.path.exists(path)):
             p = pickle.Unpickler(open(path, 'rb'))
             self.db = p.load()
+            for k in self.db.keys():
+                self.log.info("%s:%s" % (k, self.db[k].price))
         else:
             raise Exception("Vend DB Not Found")
 
